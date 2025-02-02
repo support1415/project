be99b314d3e8c0f5944b6e155ea21125ecbb84cd
support1415 <jiwony1027@gmail.com>
2025-02-02 14:19:15 +0900

    게시글, 댓글 작성

---
+++ projectsite/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from projectsite.models import Article, Comment
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
+        labels = {
+            'title': '제목',
+            'content': '내용',
+        }  
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '내용',
+        }
--- projectsite/urls.py
+++ projectsite/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'projectsite'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:article_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('comment/create/<int:article_id>/', views.comment_create, name='comment_create'),
+    path('article/create/', views.article_create, name='article_create'),
 ]
